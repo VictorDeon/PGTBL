de1a0dedb73729b2907d0a44b7dfde4d551ad1e2
Victor Arnaud <victorhad@gmail.com>
2017-12-10 18:50:45 -0200

    Test to see list of available user to insert into discipline

--- disciplines/tests/test_insert_students.py
+++ disciplines/tests/test_insert_students.py
@@ -10,9 +10,9 @@ from core.test_utils import (
 User = get_user_model()
 
 
-class RemoveStudentsTestCase(TestCase):
+class ListUsersTestCase(TestCase):
     """
-    Test to remove students or monitors from discipline.
+    Test case to list all available users that can be inserted into discipline
     """
 
     def setUp(self):
@@ -22,7 +22,109 @@ class RemoveStudentsTestCase(TestCase):
 
         self.client = Client()
         self.teacher = user_factory(name='Pedro')
-        self.teachers = user_factory(qtd=4)
+        self.teachers = user_factory(qtd=2)
+        self.student = user_factory(name='Maria', is_teacher=False)
+        self.monitor = user_factory(name='João')
+        self.students = user_factory(
+            qtd=9,
+            is_teacher=False
+        )
+        self.discipline = mommy.make(
+            Discipline,
+            teacher=self.teacher,
+            title='Discipline04',
+            course='Engineering',
+            password='12345',
+            is_closed=True,
+            students_limit=10,
+            monitors_limit=3,
+            students=self.students,
+            monitors=self.teachers,
+            make_m2m=True
+        )
+        self.url = reverse_lazy(
+            'disciplines:users',
+            kwargs={'slug': self.discipline.slug}
+        )
+        self.client.login(username=self.teacher.username, password='test1234')
+
+    def tearDown(self):
+        """
+        This method will run after any test.
+        """
+
+        Discipline.objects.all().delete()
+        User.objects.all().delete()
+
+    def test_only_teacher_can_see_page_add_students(self):
+        """
+        Test only teacher can see the add students or monitors page.
+        """
+
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.try_to_access_url(self.student)
+        self.try_to_access_url(self.students[0])
+        self.try_to_access_url(self.monitor)
+        self.try_to_access_url(self.teachers[0])
+
+    def try_to_access_url(self, user):
+        """
+        Try to access url with message error e redirect to profile.
+        """
+
+        self.client.logout()
+        redirect_url = reverse_lazy('accounts:profile')
+        self.client.login(username=user.username, password='test1234')
+        response = self.client.get(self.url, follow=True)
+        self.assertRedirects(response, redirect_url)
+        check_messages(
+            self, response,
+            tag='alert-danger',
+            content="You are not authorized to do this action."
+        )
+
+    def test_redirect_to_login(self):
+        """
+        Try to acess this page without logged in.
+        """
+
+        self.client.logout()
+        response = self.client.get(self.url)
+        login_url = reverse_lazy('accounts:login')
+        redirect_url = '{0}?next={1}'.format(login_url, self.url)
+        self.assertRedirects(response, redirect_url)
+
+    def test_only_specific_users_can_be_inserted_into_discipline(self):
+        """
+        Test only users that are not students or monitors from discipline or
+        user that are not the teacher of discipline can be listed to be
+        inserted into discipline.
+        """
+
+        response = self.client.get(self.url)
+        self.assertEqual(self.discipline.students.count(), 9)
+        self.assertEqual(self.discipline.monitors.count(), 2)
+        paginator = response.context['paginator']
+        # Only 1 student and 2 teacher are available to inserted
+        self.assertEqual(paginator.count, 2)
+        self.assertEqual(paginator.per_page, 12)
+        self.assertEqual(paginator.num_pages, 1)
+
+
+class InsertStudentsTestCase(TestCase):
+    """
+    Test case to insert students or monitors to discipline by teacher.
+    """
+
+    def setUp(self):
+        """
+        This method will run before any test case.
+        """
+
+        self.client = Client()
+        self.teacher = user_factory(name='Pedro')
+        self.teachers = user_factory(qtd=2)
         self.student = user_factory(name='Maria', is_teacher=False)
         self.monitor = user_factory(name='Caio')
         self.students = user_factory(
@@ -39,7 +141,7 @@ class RemoveStudentsTestCase(TestCase):
             students_limit=10,
             monitors_limit=3,
             students=self.students,
-            monitors=self.teachers[2:],
+            monitors=self.teachers,
             make_m2m=True
         )
 
@@ -117,7 +219,7 @@ class RemoveStudentsTestCase(TestCase):
         """
 
         self.client.login(
-            username=self.teachers[1].username,
+            username=self.teachers[0].username,
             password='test1234'
         )
         redirect_url = reverse_lazy('accounts:profile')
@@ -174,6 +276,25 @@ class RemoveStudentsTestCase(TestCase):
             self.monitor, 9, 2, redirect_url,
             tag='alert-danger',
             msg='You are not authorized to do this action.'
+        )
+
+    def test_teacher_can_not_insert_yourself_into_discipline(self):
+        """
+        Teacher can't insert yourself into discipline.
+        """
+
+        self.client.login(
+            username=self.teacher.username,
+            password='test1234'
+        )
+        redirect_url = reverse_lazy(
+            'disciplines:users',
+            kwargs={'slug': self.discipline.slug}
+        )
+        self.insert_student(
+            self.teacher, 9, 2, redirect_url,
+            tag='alert-danger',
+            msg="You can't get into your own discipline."
         )
 
     def insert_student(self, user, students, monitors, redirect_url, tag, msg):
