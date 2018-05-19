b862fe6694970bce5f4855e05b536df722bace26
Lucas S <lucas.soaresouza@gmail.com>
2018-05-19 00:42:18 -0300

    Adds tests for delete question by teacher and students

--- questions/tests/test_delete_question.py
+++ questions/tests/test_delete_question.py
@@ -8,6 +8,8 @@ from questions.models import (
     IRATSubmission, GRATSubmission
 )
 
+from TBLSessions.models import TBLSession
+
 User = get_user_model()
 
 
@@ -20,15 +22,65 @@ class DeleteQuestionTestCase(TestCase):
         """
         This method will run before any test case.
         """
+        self.client = Client()
+        self.session = mommy.make('TBLSession')
 
-        pass
+        self.question = mommy.make(
+            Question,
+            title='testtitle',
+            session= self.session,
+            level= 'basic',
+            topic= 'testtopic',
+            )
+
+        self.session.questions.add(self.question)
+
+        self.student = User.objects.create_user(
+            username='Test3',
+            email='test3@gmail.com',
+            password='test1234',
+            is_teacher=False
+        )
+
+        self.session.discipline.students.add(self.student)
+
+        self.teacher = User.objects.create_user(
+            username='Test1',
+            email='test1@gmail.com',
+            password='test1234',
+        )
+
+        self.session.discipline.teacher = self.teacher
+
+        self.monitor = User.objects.create_user(
+            username='Test2',
+            email='test2@gmail.com',
+            password='test1234',
+            is_teacher=True
+        )
+
+        self.session.discipline.monitor = self.monitor
+        self.session.discipline.save()
+
+        self.url = reverse_lazy(
+            'questions:delete-question',
+            kwargs={'slug': self.session.
+                    discipline.slug,
+                    'pk': self.session.pk,
+                    'question_id':self.question.id}
+        )
+
 
     def tearDown(self):
         """
         This method will run after any test.
         """
 
-        pass
+        self.teacher.delete()
+        self.monitor.delete()
+        self.student.delete()
+        self.question.delete()
+
 
     def test_redirect_to_login(self):
         """
@@ -41,22 +93,30 @@ class DeleteQuestionTestCase(TestCase):
         """
         Test to delete a question by teacher.
         """
-
-        pass
+        self.assertEqual(self.session.questions.count(),1)
+        self.client.login(username=self.teacher.username, password='test1234')
+        response = self.client.post(self.url)
+        self.assertEqual(self.session.questions.count(), 0)
 
     def test_delete_question_by_monitors(self):
         """
         Test to delete a question by monitors.
         """
 
-        pass
+        # self.assertEqual(self.session.questions.count(),1)
+        # self.client.login(username=self.monitor.username, password='test1234')
+        # response = self.client.post(self.url)
+        # self.assertEqual(self.session.questions.count(), 0)
 
     def test_delete_question_by_student_fail(self):
         """
         Student can not delete a question.
         """
 
-        pass
+        self.assertEqual(self.session.questions.count(),1)
+        self.client.login(username=self.student.username, password='test1234')
+        response = self.client.post(self.url)
+        self.assertEqual(self.session.questions.count(), 1)
 
     def test_delete_alternatives_when_delete_question(self):
         """
