d3522d8d41c307209e9f1a99d3027cec3eb865e7
CamilaFerrer <camila.c.ferrer@gmail.com>
2018-05-19 17:02:37 -0300

    HTML added to peer review views

--- peer_review/views.py
+++ peer_review/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def peer(request):
-    return HttpResponse("Tela da Avaliação em Pares")
+    return render(request, 'peer_review/peer.html', {})
