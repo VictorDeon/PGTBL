5a91487b9046aa03e2dd961aa382ed4dd87667cc
Alax Alves <alaxallves@gmail.com>
2018-05-20 19:05:14 -0300

    Testing TBL Sessions practical details and practical update

--- TBLSessions/tests/test_practical_detail.py
+++ TBLSessions/tests/test_practical_detail.py
@@ -17,27 +17,61 @@ class ShowPracticalTestCase(TestCase):
         """
         This method will run before any test case.
         """
+        self.client = Client()
+        self.teacher = User.objects.create_user(
+            username='someTeacher',
+            email='teacherEmail@email.com',
+            password='somepass'
+            is_teacher=True
+        )
 
-        pass
+        self.discipline = mommy.make('Discipline')
+        self.discipline.monitor.add(self.teacher)
+
+        self.monitor = User.objects.create_user(
+            username='someMonitor',
+            email='monitorEmail@email.com',
+            password='somepass'
+            is_teacher=True
+        )
+
+        self.discipline = mommy.make('Discipline')
+        self.discipline.monitor.add(self.monitor)
+
+        self.tbl_sessions = mommy.make(
+            TBLSession,
+            discipline=self.discipline,
+            _quantity=30
+        )
+        self.tbl_session = self.tbl_sessions[0]
 
     def tearDown(self):
         """
         This method will run after any test.
         """
-
-        pass
+        self.teacher.delete()
+        self.monitor.delete()
+        self.tbl_session.delete()
 
     def test_show_practical_test_to_student(self):
         """
         The practical test need to be opened by teacher for student to see
         """
+        url = '/practical-test'
 
-        pass
-
+        successful_response = self.teacher.get(url, follow=True)
+        self.assertEqual(successful_response.status_code, 200)        
+        
     def test_teacher_and_monitor_can_see_practical_test(self):
         """
         Teacher and monitors that is a teacher can see the practical test,
         before it being opened.
         """
+        url = '/practical-test'
 
-        pass
+        successful_response_teacher = self.teacher.get(url, follow=True)
+        successful_response_monitor = self.teacher.get(url, follow=True)
+        
+        self.assertEqual(successful_response_teacher.status_code, 200)
+        self.assertEqual(successful_response_monitor.status_code, 200)
+        
