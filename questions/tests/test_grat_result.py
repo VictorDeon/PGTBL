e21b050b327eb2410724cbb716054ca8abb41a67
Bernardo Henrique <bernardohrl.unb@gmail.com>
2018-05-20 20:48:36 -0300

    Test if user can see grat result
    Co-authored-by: Emanoel Belchior <belchior.emanoel@gmail.com>

--- questions/tests/test_grat_result.py
+++ questions/tests/test_grat_result.py
@@ -20,31 +20,64 @@ class GRATResultTestCase(TestCase):
         """
         This method will run before any test case.
         """
+        # Preparing TBL Section
+        self.client = Client()
+        self.student = User.objects.create_user(
+            username='estudante',
+            email='estudante@email.com',
+            password='estudate123'
+        )
+        self.tbl_session = mommy.make('TBLSession')
+        self.tbl_session.discipline.students.add(self.student)
 
-        pass
+
 
     def tearDown(self):
         """
         This method will run after any test.
         """
+        self.student.delete()
+        self.tbl_session.delete()
 
-        pass
+
+
+
 
     def test_user_can_see_grat_result(self):
         """
         User like student, teacher and monitors can see the result of grat
         after the test is over or if student finish the test.
         """
+        # /profile/materia/sessions/pk/grat/result
+        url = '/profile/{}/sessions/{}/grat/result'.format(
+            self.tbl_session.discipline.slug,
+            self.tbl_session.pk
+        )
+        
+        self.client.login(
+            username=self.student.username,
+            password='estudante123'
+        )
 
-        pass
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+
 
     def test_show_only_not_exercise_question(self):
         """
         Show only not exercise question that are into grat test, and show the
         answers.
         """
-
         pass
+
+
+
+
+        
+        
+
+
 
     def test_calcule_the_grat_result(self):
         """
@@ -52,8 +85,9 @@ class GRATResultTestCase(TestCase):
         score that the group made, total of scores and grade of group.
         Only students have grade created.
         """
-
         pass
+        
+        
 
     def test_group_submit_test_only_once(self):
         """
