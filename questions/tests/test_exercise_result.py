532dfcb0e556e2017a6c727b63c90dd9ff1007bf
Arthur Diniz <arthurbdiniz@gmail.com>
2018-05-20 19:29:25 -0300

    Increment setUp and tearDown methods
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- questions/tests/test_exercise_result.py
+++ questions/tests/test_exercise_result.py
@@ -1,12 +1,16 @@
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
-from core.test_utils import check_messages
+from core.test_utils import check_messages, user_factory
 from model_mommy import mommy
 from questions.models import (
     Question, Alternative, ExerciseSubmission,
     IRATSubmission, GRATSubmission
 )
+from disciplines.models import Discipline
+from TBLSessions.models import TBLSession
+from questions.models import Question
+
 
 User = get_user_model()
 
@@ -21,19 +25,87 @@ class ExerciseResultTestCase(TestCase):
         This method will run before any test case.
         """
 
-        pass
+        self.user = User.objects.create_user(
+            username='test',
+            email='test@test.com',
+            password='password'
+        )
+
+        self.client = Client()
+        self.teacher = user_factory(name='Pedro')
+        self.teachers = user_factory(qtd=2)
+        self.student = user_factory(name='Maria', is_teacher=False)
+        self.monitor = user_factory(name='João')
+        self.students = user_factory(
+            qtd=9,
+            is_teacher=False
+        )
+        self.discipline = mommy.make(
+            Discipline,
+            teacher=self.teacher,
+            title='DisciplineVV',
+            course='Software Engineering',
+            password='12345',
+            is_closed=False,
+            students_limit=10,
+            monitors_limit=3,
+            students=self.students,
+            monitors=self.teachers,
+            make_m2m=True
+        )
+        self.session = mommy.make(
+            TBLSession,
+            title="TBL1",
+            description="First TBL Session",
+            is_closed=False,
+            make_m2m=True
+        )
+        self.question = mommy.make(
+            Question,
+            title="Question 1",
+            session=self.session,
+            level="Basic",
+            topic="What is the result between the sum of 1 + 1?",
+            is_exercise=True
+        )
+        self.url = reverse_lazy(
+            'questions:list',
+            kwargs={
+                'slug': self.discipline.slug,
+                'pk': self.session.id,
+            }
+        )
 
     def tearDown(self):
         """
         This method will run after any test.
         """
 
-        pass
+        User.objects.all().delete()
+        Discipline.objects.all().delete()
+        TBLSession.objects.all().delete()
+
 
     def test_user_can_see_exercise_result(self):
         """
         User like student, teacher and monitors can see the result of exercise.
         """
+
+        self.client.login(username="test", password="password")
+        url = '/profile/{}/sessions/1/questions/exercises/result/'.format(self.session.discipline.slug)
+
+        # response = self.client.get(url, follow=True)
+        # redirect_url, status_code = response.redirect_chain[-1]
+        #
+        # self.assertEqual(status_code, 302)
+        # self.assertEqual(redirect_url, '/profile/')
+        #
+        # if '<h1>Login</h1>' in str(response.content):
+        #     is_logged = False
+        # else:
+        #     is_logged = True
+        #
+        # self.assertIs(is_logged, True)
 
         pass
 
