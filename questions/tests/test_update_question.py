0565a9880effe696095f02bcd82e3c4ae0645e2f
João Pedro Sconetto <sconetto.joao@gmail.com>
2018-05-20 20:47:08 -0300

    Adds test to update question
    Add the test to only one alternative correct
    
    Add the test to missing one alternative

--- questions/tests/test_update_question.py
+++ questions/tests/test_update_question.py
@@ -1,14 +1,14 @@
-from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
-from django.urls import resolve, reverse
-from core.test_utils import check_messages
-from questions import views_question
 from model_mommy import mommy
+from TBLSessions.models import TBLSession
+from disciplines.models import Discipline
 from questions.models import (
-    Question, Alternative, ExerciseSubmission,
-    IRATSubmission, GRATSubmission
+    Question, Alternative
 )
+from TBLSessions.models import TBLSession
+from disciplines.models import Discipline
 
 User = get_user_model()
 
@@ -22,9 +22,16 @@ class UpdateQuestionTestCase(TestCase):
         """
         This method will run before any test case.
         """
-        self.question = mommy.make('Question')
         self.client = Client()
+        self.question = mommy.make('Question')
         self.session = mommy.make('TBLSession')
+
+        self.alternatives = mommy.make('Alternative', _quantity=4)
+        self.alternatives[0].is_correct = True
+        self.alternatives[0].save()
+
+        self.session.questions.add(self.question)
+        self.question.alternatives.set(self.alternatives)
 
         self.student = User.objects.create_user(
             username='student',
@@ -50,18 +57,22 @@ class UpdateQuestionTestCase(TestCase):
         self.session.discipline.teacher = self.teacher
         self.session.discipline.save()
 
-        self.url = reverse('questions:create-question',
+        self.url = reverse('questions:update-question',
                            kwargs={'slug': self.session.discipline.slug,
-                                   'pk': self.session.pk})
+                                   'pk': self.session.pk,
+                                   'question_id': self.question.id})
 
 
     def tearDown(self):
         """
         This method will run after any test.
         """
-        self.question.delete()
-        self.monitor.delete()
-        self.teacher.delete()
+        User.objects.all().delete()
+        Question.objects.all().delete()
+        TBLSession.objects.all().delete()
+        Discipline.objects.all().delete()
+        Alternative.objects.all().delete()
+
 
 
     def test_redirect_to_login(self):
@@ -75,9 +86,7 @@ class UpdateQuestionTestCase(TestCase):
             self.question.id
         )
 
-        print(url)
         response = self.client.get(url,follow=True)
-        print(response.redirect_chain)
         self.assertRedirects(response, '/login/?next='+url, status_code=302,
             target_status_code=200,fetch_redirect_response=True)
 
@@ -121,7 +130,6 @@ class UpdateQuestionTestCase(TestCase):
         }
 
         response = self.client.get(url, follow=True)
-        print(response.redirect_chain)
         self.assertRedirects(response, '/login/?next='+url, status_code=302,
             target_status_code=200,fetch_redirect_response=True)
 
@@ -152,7 +160,40 @@ class UpdateQuestionTestCase(TestCase):
         with correct answer, can not be 4, 3, or 2 correct answer.
         """
 
-        pass
+        self.client.login(username=self.teacher.username, password='test1234')
+
+        data = {
+            'alternatives-TOTAL_FORMS': '4',
+            'alternatives-INITIAL_FORMS': '0',
+            'alternatives-MAX_NUM_FORMS': '4',
+
+            'title': "Questao 01",
+            'topic': "Testes de Software",
+            'level': 'Basic',
+            'is_exercise': False,
+
+            'alternatives-0-title': "A",
+            'alternatives-0-is_correct': True,
+
+            'alternatives-1-title': "B",
+            'alternatives-1-is_correct': True,
+
+            'alternatives-2-title': "C",
+            'alternatives-2-is_correct': True,
+
+            'alternatives-3-title': "D",
+            'alternatives-3-is_correct': False,
+
+        }
+
+        response = self.client.post(self.url, data=data)
+        alter = response.context_data['alternatives']
+
+        self.assertIsNotNone(response.context_data['alternatives'][0]._errors)
+        self.assertIsNotNone(response.context_data['alternatives'][1]._errors)
+        self.assertIsNotNone(response.context_data['alternatives'][2]._errors)
+        self.assertIsNotNone(response.context_data['alternatives'][3]._errors)
+        self.assertEqual(Alternative.objects.all().count(), 4)
 
     def test_update_four_alternatives_in_a_question(self):
         """
@@ -160,4 +201,34 @@ class UpdateQuestionTestCase(TestCase):
         with 3 or 2 or 1 alternative.
         """
 
-        pass
+        self.client.login(username=self.teacher.username, password='test1234')
+
+        data = {
+            'alternatives-TOTAL_FORMS': '4',
+            'alternatives-INITIAL_FORMS': '0',
+            'alternatives-MAX_NUM_FORMS': '4',
+
+            'title': "Questao 01",
+            'topic': "Testes de Software",
+            'level': 'Basic',
+            'is_exercise': False,
+
+            'alternatives-0-title': "A",
+            'alternatives-0-is_correct': True,
+
+            'alternatives-1-title': "B",
+            'alternatives-1-is_correct': False,
+
+            'alternatives-2-title': "C",
+            'alternatives-2-is_correct': False,
+
+            'alternatives-3-title': "",
+            'alternatives-3-is_correct': None,
+        }
+
+        response = self.client.post(self.url, data=data)
+        self.assertIsNotNone(response.context_data['alternatives'][0]._errors)
+        self.assertIsNotNone(response.context_data['alternatives'][1]._errors)
+        self.assertIsNotNone(response.context_data['alternatives'][2]._errors)
+        self.assertIsNotNone(response.context_data['alternatives'][3]._errors)
+        self.assertEqual(Alternative.objects.all().count(), 4)
