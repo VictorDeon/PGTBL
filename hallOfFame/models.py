56356b1313f916f67f05708b5aea493b064242ad
williamelias <williameliasalves@gmail.com>
2018-05-20 21:10:02 -0300

    #13 - Removal of the alternative branch and creation of the base model hall.

--- hallOfFame/models.py
+++ hallOfFame/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class HallOfFame(models.Model):
+
+    discipline = models.OneToOneField(
+        Discipline,
+        on_delete=models.CASCADE,
+        verbose_name='Discipline',
+        related_name='hall'
+    )
