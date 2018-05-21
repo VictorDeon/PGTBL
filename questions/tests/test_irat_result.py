4e20364925b876d0894630e889b9368b654b013e
Wallacy Braz <wallacy.braz@live.com>
2018-05-21 14:42:39 -0300

    Implement show_only_not_exercises_questions test

--- questions/tests/test_irat_result.py
+++ questions/tests/test_irat_result.py
@@ -1,7 +1,9 @@
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
-from core.test_utils import check_messages
+from core.test_utils import (
+    check_messages, user_factory
+)
 from core.roles import Teacher
 from model_mommy import mommy
 from questions.views_irat import (
@@ -69,8 +71,61 @@ class IRATResultTestCase(TestCase):
         Show only not exercise question that are into irat test, and show the
         answers.
         """
+        teacher_test = user_factory(name='Wallacy',password='passwordtest',is_teacher=True)
 
-        pass
+
+        discipline_test = mommy.make(
+                Discipline,
+                teacher = teacher_test,
+                title='Calculus 2',
+                course='Math',
+                password='1234',
+                classroom='Class C'
+        )
+
+
+        session_test = mommy.make(
+                TBLSession,
+                discipline=discipline_test,
+                title='TBL4',
+                irat_datetime=timezone.localtime(timezone.now()),
+                irat_weight=3,
+                irat_duration=30
+        )
+
+        exercise1 = mommy.make(Question,session=session_test,is_exercise=True)
+
+        exercise2 = mommy.make(Question,session=session_test,is_exercise=True)
+
+        exercise3 = mommy.make(Question,session=session_test,is_exercise=True)
+
+        question1 = mommy.make(Question,session=session_test,is_exercise=False)
+
+        question2 = mommy.make(Question,session=session_test,is_exercise=False)
+
+        question3 = mommy.make(Question,session=session_test,is_exercise=False)
+
+        self.client.login(username=teacher_test.username, password='passwordtest')
+
+        question_list = [question1,question2,question3]
+        exercise_list = [exercise1,exercise2,exercise3]
+
+
+        url = '/profile/{}/sessions/{}/irat/'.format(session_test.discipline.slug, session_test.id)
+
+        response = self.client.get(url)
+
+        p = response.context['view'].get_queryset()
+
+        query_list = list(p)
+
+        for question in question_list:
+            self.assertEquals(question in query_list,True)
+
+        for exercise in exercise_list:
+            self.assertEquals(exercise in query_list,False)
+
+
 
     def test_calcule_the_irat_result(self):
 
