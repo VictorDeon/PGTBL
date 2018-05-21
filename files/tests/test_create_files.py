0d76b3ad7040c402a9ec3947e9612d17e50c5893
lugope <lucasgpereira.10@gmail.com>
2018-05-21 12:54:08 -0300

    Creting tests to CreateFileTestCase.

--- files/tests/test_create_files.py
+++ files/tests/test_create_files.py
@@ -17,50 +17,113 @@ class CreateFileTestCase(TestCase):
         """
         This method will run before any test case.
         """
+        self.client = Client()
 
-        pass
+        # Create user
+        self.user = User.objects.create_user(
+            username='Testuser',
+            email='user@email.com',
+            password='tester123')
+
+        #Create teacher
+        self.teacher = User.objects.create_user(
+            username='Testteacher',
+            email='teacher@email.com',
+            password='tester123',
+            is_teacher=True
+        )
+
+        #Create monitor
+        self.monitor = User.objects.create_user(
+            username='Testmonitor',
+            email='monitor@email.com',
+            password='tester123',
+            is_teacher=True
+        )
+
+        #Create discipline
+        self.discipline = mommy.make('Discipline')
+        self.discipline.teacher = self.teacher
+        self.tbl_sessions = mommy.make(
+            TBLSession,
+            discipline=self.discipline,
+            _quantity=15
+        )
+        self.tbl_session = self.tbl_sessions[0]
+
+        #Create file url
+        self.url_file_create = reverse('files:create')
 
     def tearDown(self):
         """
         This method will run after any test.
         """
-
-        pass
+        self.client.logout()
+        self.user.delete()
 
     def test_redirect_to_login(self):
         """
         User can not create a new file without logged in.
         """
+        response = self.client.get(self.url_file_create)
+        url_login = reverse('accounts:login')
+        redirect_url = url_login + '?next=' + self.url_file_create
 
-        pass
+        self.assertTrue(response.status_code, 302)
+        self.assertRedirects(response, redirect_url, 302)
+        
 
     def test_create_file_by_teacher(self):
         """
         Test to create a new file by teacher.
         """
+        self.client.login(username=self.teacher.username, password='tester123')
+        file_before = File.objects.all().count()
+        response = self.client.create(self.url_file_create)
+        res = self.client.get(url_file_create)
+        file_after = File.objects.all().count()
 
-        pass
+        test = file_before < file_after
+        self.assertTrue(test)
 
     def test_create_file_by_monitors(self):
         """
         Test to create a new file by monitors.
         """
+        self.client.login(username=self.monitor.username, password='tester123')
+        file_before = File.objects.all().count()
+        response = self.client.create(self.url_file_create)
+        res = self.client.get(url_file_create)
+        file_after = File.objects.all().count()
 
-        pass
+        test = file_before < file_after
+        self.assertTrue(test)
 
     def test_create_file_fail(self):
         """
         User can not create a file with invalid fields.
         """
+        self.client.login(username=self.teacher.username, password='tester123')
+        file_before = File.objects.all().count()
+        response = self.client.create(self.url_file_create)
+        res = self.client.get(url_file_create)
+        file_after = File.objects.all().count()
 
-        pass
+        test = file_before == file_after
+        self.assertTrue(test)
 
     def test_create_file_by_student_fail(self):
         """
         Student can not create a file.
         """
+        self.client.login(username=self.user.username, password='tester123')
+        file_before = File.objects.all().count()
+        response = self.client.create(self.url_file_create)
+        res = self.client.get(url_file_create)
+        file_after = File.objects.all().count()
 
-        pass
+        test = file_before == file_after
+        self.assertTrue(test)
 
 
 class CreateSessionFileTestCase(TestCase):
@@ -72,8 +135,42 @@ class CreateSessionFileTestCase(TestCase):
         """
         This method will run before any test case.
         """
+        self.client = Client()
 
-        pass
+        # Create user
+        self.user = User.objects.create_user(
+            username='Testuser',
+            email='user@email.com',
+            password='tester123')
+
+        #Create teacher
+        self.teacher = User.objects.create_user(
+            username='Testteacher',
+            email='teacher@email.com',
+            password='tester123',
+            is_teacher=True
+        )
+
+        #Create monitor
+        self.monitor = User.objects.create_user(
+            username='Testmonitor',
+            email='monitor@email.com',
+            password='tester123',
+            is_teacher=True
+        )
+
+        #Create discipline
+        self.discipline = mommy.make('Discipline')
+        self.discipline.teacher = self.teacher
+        self.tbl_sessions = mommy.make(
+            TBLSession,
+            discipline=self.discipline,
+            _quantity=15
+        )
+        self.tbl_session = self.tbl_sessions[0]
+
+        #Create file url
+        self.url_file_create = reverse('files:session-create')
 
     def tearDown(self):
         """
@@ -86,8 +183,12 @@ class CreateSessionFileTestCase(TestCase):
         """
         User can not create a new file without logged in.
         """
+        response = self.client.get(self.url_file_create)
+        url_login = reverse('accounts:login')
+        redirect_url = url_login + '?next=' + self.url_file_create
 
-        pass
+        self.assertTrue(response.status_code, 301)
+        self.assertRedirects(response, redirect_url, 302)
 
     def test_create_file_by_teacher(self):
         """
