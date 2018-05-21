e1b19196af5ea1bf1748b4d39c7e24a46bffd08a
João Egewarth <egewarth@gmail.com>
2018-05-20 21:32:28 -0300

    WIP: Começando a fazer os testes de update files

--- files/tests/test_update_files.py
+++ files/tests/test_update_files.py
@@ -17,27 +17,70 @@ class UpdateFileTestCase(TestCase):
         """
         This method will run before any test case.
         """
+        # Create user
+        self.user = User.objects.create_user(
+            username='tester',
+            email='tester@tester.com',
+            password='tester123')
 
-        pass
+        # Create teacher
+        self.teacher = User.objects.create_user(
+            username='someTeacher',
+            email='teacherEmail@email.com',
+            password='somepass'
+            is_teacher=True
+        )
+
+        # Create discipline and discipline file
+        self.discipline = mommy.make('Discipline')
+        self.discipline.students.add(self.user)
+        self.discipline_file = DisciplineFile.objects.create(
+            extension='.jpg',
+            title='test',
+            archive='test.jpg',
+            discipline=self.discipline)
+
+        # Setup client and base url
+        self.client = Client()
+        self.url = reverse('files:list', args=[self.discipline.slug])
+
 
     def tearDown(self):
         """
         This method will run after any test.
         """
 
-        pass
+        self.client.logout()
+        self.user.delete()
+
 
     def test_redirect_to_login(self):
         """
         User can not update a file without logged in.
         """
+        response = self.client.get(self.url)
+        url_login = reverse('accounts:login')
+        redirect_url = url_login + '?next=' + self.url
 
-        pass
+        self.assertTrue(response.status_code, 302)
+        self.assertRedirects(response, redirect_url, 302)
+
 
     def test_update_file_by_teacher(self):
         """
         Test to update a file by teacher.
         """
+        self.teacher.login(
+            username=self.teacher.username,
+            password='somepass'
+        )
+
+        url = '/edit/'
+
+        successful_response = self.teacher.get(url, follow=True)
+
+        self.assertEqual(successful_response.status_code, 200)
+
 
         pass
 
