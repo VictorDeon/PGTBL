2cdaa8b14c0a72541843f59251362c29185c923d
Sannya Arvelos <sannyasantana@gmail.com>
2018-05-21 01:33:12 -0300

    Testing create question with invalid field failure.

--- questions/tests/test_update_question.py
+++ questions/tests/test_update_question.py
@@ -1,14 +1,14 @@
-from django.shortcuts import reverse
+from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
+from django.urls import resolve, reverse
+from core.test_utils import check_messages
+from questions import views_question
 from model_mommy import mommy
-from TBLSessions.models import TBLSession
-from disciplines.models import Discipline
 from questions.models import (
-    Question, Alternative
+    Question, Alternative, ExerciseSubmission,
+    IRATSubmission, GRATSubmission
 )
-from TBLSessions.models import TBLSession
-from disciplines.models import Discipline
 
 User = get_user_model()
 
@@ -22,16 +22,9 @@ class UpdateQuestionTestCase(TestCase):
         """
         This method will run before any test case.
         """
-        self.client = Client()
         self.question = mommy.make('Question')
+        self.client = Client()
         self.session = mommy.make('TBLSession')
-
-        self.alternatives = mommy.make('Alternative', _quantity=4)
-        self.alternatives[0].is_correct = True
-        self.alternatives[0].save()
-
-        self.session.questions.add(self.question)
-        self.question.alternatives.set(self.alternatives)
 
         self.student = User.objects.create_user(
             username='student',
@@ -60,19 +53,16 @@ class UpdateQuestionTestCase(TestCase):
         self.url = reverse('questions:update-question',
                            kwargs={'slug': self.session.discipline.slug,
                                    'pk': self.session.pk,
-                                   'question_id': self.question.id})
+                                   'question_id':self.question.id})
 
 
     def tearDown(self):
         """
         This method will run after any test.
         """
-        User.objects.all().delete()
-        Question.objects.all().delete()
-        TBLSession.objects.all().delete()
-        Discipline.objects.all().delete()
-        Alternative.objects.all().delete()
-
+        self.question.delete()
+        self.monitor.delete()
+        self.teacher.delete()
 
 
     def test_redirect_to_login(self):
@@ -116,7 +106,6 @@ class UpdateQuestionTestCase(TestCase):
         #print(response.redirect_chain)
         self.assertEqual(self.question.title, "Questao 01")
 
-
     def test_update_question_by_monitors(self):
         """
         Test to update a question and alternatives by monitors.
@@ -147,8 +136,24 @@ class UpdateQuestionTestCase(TestCase):
         """
         User can not update a question with invalid fields and alternativas.
         """
+        url_question = '/profile/{}/sessions/{}/questions/{}/edit/'.format(
+            self.question.session.discipline.slug,
+            self.question.session.discipline.pk,
+            self.question.id
+        )
 
-        pass
+        self.client.login(username=self.monitor.username, password='test1234')
+
+        data = {
+            'title': 'Questao 01',
+            'topic': '',
+            'level': 'Basic',
+            'is_exercise': True,
+        }
+
+        response = self.client.put(url_question, data=data)
+
+        self.assertNotEqual(self.question.title, "Questao 01")
 
     def test_create_question_by_student_fail(self):
         """
@@ -163,40 +168,7 @@ class UpdateQuestionTestCase(TestCase):
         with correct answer, can not be 4, 3, or 2 correct answer.
         """
 
-        self.client.login(username=self.teacher.username, password='test1234')
-
-        data = {
-            'alternatives-TOTAL_FORMS': '4',
-            'alternatives-INITIAL_FORMS': '0',
-            'alternatives-MAX_NUM_FORMS': '4',
-
-            'title': "Questao 01",
-            'topic': "Testes de Software",
-            'level': 'Basic',
-            'is_exercise': False,
-
-            'alternatives-0-title': "A",
-            'alternatives-0-is_correct': True,
-
-            'alternatives-1-title': "B",
-            'alternatives-1-is_correct': True,
-
-            'alternatives-2-title': "C",
-            'alternatives-2-is_correct': True,
-
-            'alternatives-3-title': "D",
-            'alternatives-3-is_correct': False,
-
-        }
-
-        response = self.client.post(self.url, data=data)
-        alter = response.context_data['alternatives']
-
-        self.assertIsNotNone(response.context_data['alternatives'][0]._errors)
-        self.assertIsNotNone(response.context_data['alternatives'][1]._errors)
-        self.assertIsNotNone(response.context_data['alternatives'][2]._errors)
-        self.assertIsNotNone(response.context_data['alternatives'][3]._errors)
-        self.assertEqual(Alternative.objects.all().count(), 4)
+        pass
 
     def test_update_four_alternatives_in_a_question(self):
         """
@@ -204,34 +176,4 @@ class UpdateQuestionTestCase(TestCase):
         with 3 or 2 or 1 alternative.
         """
 
-        self.client.login(username=self.teacher.username, password='test1234')
-
-        data = {
-            'alternatives-TOTAL_FORMS': '4',
-            'alternatives-INITIAL_FORMS': '0',
-            'alternatives-MAX_NUM_FORMS': '4',
-
-            'title': "Questao 01",
-            'topic': "Testes de Software",
-            'level': 'Basic',
-            'is_exercise': False,
-
-            'alternatives-0-title': "A",
-            'alternatives-0-is_correct': True,
-
-            'alternatives-1-title': "B",
-            'alternatives-1-is_correct': False,
-
-            'alternatives-2-title': "C",
-            'alternatives-2-is_correct': False,
-
-            'alternatives-3-title': "",
-            'alternatives-3-is_correct': None,
-        }
-
-        response = self.client.post(self.url, data=data)
-        self.assertIsNotNone(response.context_data['alternatives'][0]._errors)
-        self.assertIsNotNone(response.context_data['alternatives'][1]._errors)
-        self.assertIsNotNone(response.context_data['alternatives'][2]._errors)
-        self.assertIsNotNone(response.context_data['alternatives'][3]._errors)
-        self.assertEqual(Alternative.objects.all().count(), 4)
+        pass
