37659aeeb2d54d8468afee816851c5d20e72d8b8
CamilaFerrer <camila.c.ferrer@gmail.com>
2018-05-23 12:38:36 -0300

    Editing peer review view.
    Co-authored-by: GabrielOak <gabriel315df@gmail.com>
    Co-authored-by: ritzare <joaovictor-fza@hotmail.com>
    Co-authored-by: MatheusFilipe95 <mafieu666@hotmail.com>
    Co-authored-by: Nief13 <vinicius.c.f@hotmail.com>
    Co-authored-by: Alexandree-Santos <xandy03@gmail.com>

--- peer_review/views.py
+++ peer_review/views.py
@@ -1,5 +1,47 @@
 from django.shortcuts import render
+# Django imports
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user_model
+from django.views.generic import (
+    CreateView, ListView, UpdateView, FormView, DeleteView
+)
+
+# Application imoports
+from disciplines.models import Discipline
+
+# Get the custom user from settings
+User = get_user_model()
 
 
 def peer(request):
     return render(request, 'peer_review/peer.html', {})
+
+
+def home(request):
+    return render(request, 'peer_review/teste.html', {})
+
+
+class PeerReviewView(LoginRequiredMixin, ListView):
+    """
+    Class to read a profile user and his disciplines.
+    """
+
+    paginate_by = 6
+    template_name = 'peer_review/peer.html'
+    context_object_name = 'disciplines'
+
+    def get_context_data(self, **kwargs):
+        context = super(PeerReviewView, self).get_context_data(**kwargs)
+        context['discipline'] = self.get_discipline()
+        return context
+
+    def get_discipline(self):
+        """
+        Take the discipline that the group belongs to
+        """
+
+        discipline = Discipline.objects.get(
+            slug=self.kwargs.get('slug', '')
+        )
+
+        return discipline
