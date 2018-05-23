fd42423e395f9ae4b5c8e1031a85059ff1a811dd
CamilaFerrer <camila.c.ferrer@gmail.com>
2018-05-23 15:18:38 -0300

    Modification on Sidebar

--- peer_review/urls.py
+++ peer_review/urls.py
@@ -1,10 +1,27 @@
-from django.conf.urls import url
-
+from django.conf.urls import url, include
 from . import views
 
 app_name = 'peer_review'
 
+review_patterns = [
+    url(
+        r'^teste',
+        views.home,
+        name='home'
+    ),
+    url(
+        r'^review/$',
+        views.PeerReviewView.as_view(),
+        name='review'
+    ),
+]
+
 urlpatterns = [
+    # /profile/<discipline.slug>/sessions/<session.id>/
+    url(
+        r'^profile/(?P<slug>[\w_-]+)/sessions/(?P<pk>[0-9]+)/',
+        include(review_patterns)
+    ),
     url(
         r'^peer',
         views.peer,
