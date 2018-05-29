defe0fd3ac0bca4cfe4b97deb3b23b536ddf1900
CamilaFerrer <camila.c.ferrer@gmail.com>
2018-05-29 19:31:06 -0300

    Addition of peer review admin block

--- peer_review/admin.py
+++ peer_review/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import PeerReview
 
-admin.site.register(PeerReview)
+
+class PeerReviewAdmin(admin.ModelAdmin):
+    list_display = ('username_received',
+                    'username_gave',
+                    'feedback',
+                    'score')
+    search_fields = ['username_received',
+                     'username_gave',
+                     'score']
+    list_filter = ['username_received',
+                   'username_gave',
+                   'score']
+
+
+admin.site.register(PeerReview, PeerReviewAdmin)
