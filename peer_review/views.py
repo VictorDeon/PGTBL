235b0dc29bed2fd94edd92ca93ba50ec22e7fca8
CamilaFerrer <camila.c.ferrer@gmail.com>
2018-05-30 08:32:26 -0300

    Modifying View from ListView to FormView

--- peer_review/views.py
+++ peer_review/views.py
@@ -1,57 +1,97 @@
+from django.contrib import messages
 from django.db.models import Q
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, request
+from django.shortcuts import render, redirect
+
 # Django imports
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy, reverse
 from django.views.generic import (
-    CreateView, ListView, UpdateView, FormView, DeleteView
+    ListView, FormView
 )
-from django.http import HttpResponseRedirect
 
-# Application imoports
+# Application imports
 from TBLSessions.models import TBLSession
+from core.permissions import PermissionMixin
 from disciplines.models import Discipline
+from peer_review.models import PeerReview
 from .forms import PeerReviewForm
 
 # Get the custom user from settings
 User = get_user_model()
 
 
-class PeerReviewView(LoginRequiredMixin, ListView):
+class PeerReviewView(LoginRequiredMixin,
+                     PermissionMixin,
+                     FormView):
     """
-    Class to read a profile user and his disciplines.
+    Working with form to create a peer review
     """
 
     template_name = 'peer_review/peer.html'
-    context_object_name = 'students'
+    success_url = reverse_lazy('accounts:profile')
+    form_class = PeerReviewForm
+    permissions_required = [
+        'only_student_can_change'
+    ]
+
+    def form_valid(self, form):
+
+        """
+        Receive the form already validated to create an Peer Review
+        """
+
+        session = self.get_session().id
+        username_gave = form.cleaned_data.get('username_gave')
+        username_received = form.cleaned_data.get('username_received')
+        feedback = form.cleaned_data.get('feedback')
+        score = form.cleaned_data.get('score')
+
+        peer_review = self.return_existent_review(username_gave, username_received, session)
+
+        peer_review.username_gave = username_gave
+        peer_review.username_received = username_received
+        peer_review.feedback = feedback
+        if score is None:
+            peer_review.score = 0
+        else:
+            peer_review.score = score
+        peer_review.session = session
+        peer_review.save()
+
+        return super(PeerReviewView, self).form_valid(peer_review)
+
+    def return_existent_review(self, username_gave, username_received, session):
+
+        """
+        Check if peer review already exists
+        """
+
+        try:
+            query = PeerReview.objects.get(
+                username_gave=username_gave,
+                username_received=username_received,
+                session=session,
+            )
+            peer_review = query
+        except PeerReview.DoesNotExist:
+            peer_review = PeerReview()
+
+        return peer_review
 
     def get_context_data(self, **kwargs):
         context = super(PeerReviewView, self).get_context_data(**kwargs)
         context['discipline'] = self.get_discipline()
         context['session'] = self.get_session()
+        context['students'] = self.get_all_students(self.get_discipline())
         context['form'] = PeerReviewForm()
 
         return context
 
-    def get_queryset(self):
-        """
-        List all students from discipline.
-        """
-
-        self.discipline = self.get_discipline()
-
-        students = self.discipline.students.all()
-
-        # Insert students into queryset
-        queryset = []
-        for student in students:
-            queryset.append(student)
-
-        return queryset
-
     def get_discipline(self):
         """
-        Take the discipline that the group belongs to
+        Get the specific discipline.
         """
 
         discipline = Discipline.objects.get(
@@ -74,15 +114,10 @@ class PeerReviewView(LoginRequiredMixin, ListView):
 
         return session
 
-    def get_peer_review(request):
-        if request.POST:
-            form = PeerReviewForm(request.POST)
-            if form.is_valid():
-                peer = form.save(commit=False)
-                #peer.username_received = ??
-                #peer.username_gave = User
-                peer.save()
-                #return redirect('post_detail', pk=post.pk)
-            else:
-                form = PostForm()
-    #return render(request, '#', {'form': form})
+    def get_all_students(self, discipline):
+        """
+        Get students from dicipline
+        """
+        students = discipline.students.all()
+
+        return students
