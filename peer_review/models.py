d4967999a5623878ec6e741aa32840277c814b9a
CamilaFerrer <camila.c.ferrer@gmail.com>
2018-06-03 17:09:35 -0300

    Modification in attribute names

--- peer_review/models.py
+++ peer_review/models.py
@@ -9,13 +9,13 @@ class PeerReview(models.Model):
         default=0,
     )
 
-    username_received = models.CharField(
-        _('User'),
+    student = models.CharField(
+        _('Student'),
         max_length=30,
     )
 
-    username_gave = models.CharField(
-        _('User'),
+    reviewed_by = models.CharField(
+        _('Reviewed by'),
         max_length=30,
     )
 
@@ -37,22 +37,9 @@ class PeerReview(models.Model):
         the object.
         """
 
-        return '{0}'.format(self.username_received)
+        return '{0}'.format(self.student)
 
     class Meta:
         verbose_name = _('PeerReview')
         verbose_name_plural = _('PeerReviews')
-        ordering = ['username_received', 'username_gave']
-
-class PeerReviewFormModel(models.Model):
-
-    feedback = models.TextField(
-        _('Feedback'),
-        help_text=_('Feedback about your teammate ')
-    )
-
-    score = models.PositiveSmallIntegerField(
-        _('Score'),
-        blank=True,
-        help_text=_("Score your teammate")
-    )
+        ordering = ['student', 'reviewed_by']
