226e215d00da2328e960ed27e911a86e03bc03aa
Gabriel Carvalho <gabriel315df@gmail.com>
2018-06-06 11:38:25 -0300

    Test corrections

--- peer_review/tests.py
+++ peer_review/tests.py
@@ -1,13 +1,10 @@
-from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 from peer_review.models import PeerReview
 from disciplines.models import Discipline
 from TBLSessions.models import TBLSession
 from model_mommy import mommy
-from core.test_utils import (
-    list_transform, user_factory
-)
+from core.test_utils import user_factory
 
 User = get_user_model()
 
@@ -23,13 +20,13 @@ class PeerReviewTestModel(TestCase):
         self.teacher = User.objects.create_user(
             username='Test1',
             email='test1@gmail.com',
-            password='test1234'
+            password='teacher1'
         )
 
         self.student = User.objects.create_user(
             username='Test3',
             email='test3@gmail.com',
-            password='test1234',
+            password='student1',
             is_teacher=False
         )
         self.discipline = mommy.make(
@@ -37,7 +34,7 @@ class PeerReviewTestModel(TestCase):
             teacher=self.teacher,
             title='Discipline',
             course='Engineering',
-            password='12345',
+            password='discipline1',
             students_limit=5,
             students=[self.student],
             make_m2m=True
@@ -59,7 +56,7 @@ class PeerReviewTestModel(TestCase):
 
 
         self.client.login(
-            username=self.teacher.username, password='test1234'
+            username=self.teacher.username, password='teacher1'
         )
 
     def tearDown(self):
@@ -77,7 +74,7 @@ class PeerReviewTestModel(TestCase):
 
         self.client.logout()
         self.client.login(
-            username=self.student.username, password='test1234'
+            username=self.student.username, password='student1'
         )
 
         self.assertEqual(self.peer_review.feedback, 'Feedback test')
@@ -88,7 +85,7 @@ class PeerReviewTestModel(TestCase):
 
         self.client.logout()
         self.client.login(
-            username=self.student.username, password='test1234'
+            username=self.student.username, password='student1'
         )
 
         self.assertEqual(self.peer_review.score, 10)
