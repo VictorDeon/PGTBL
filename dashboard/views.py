e1f3b610f4f2833789e8ec07a5d5cef70fa73527
CamilaFerrer <camila.c.ferrer@gmail.com>
2018-06-13 18:18:36 -0300

    Creation of Dashboard initial view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+# Django imports
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user_model
+from django.views.generic import (ListView)
 
-# Create your views here.
+# Application imports
+from core.permissions import PermissionMixin
+
+# Get the custom user from settings
+User = get_user_model()
+
+
+class DashboardView(LoginRequiredMixin,
+                    PermissionMixin,
+                    ListView):
+    """
+    Show the Dashboard page.
+    """
+
+    template_name = 'dashboard/list.html'
+    permissions_required = [
+        'only_teacher_can_change'
+    ]
+
+
