ea746644bfe565bacf5fa0a4cfd3420c9bbd3ada
williamelias <williameliasalves@gmail.com>
2018-06-25 11:29:22 -0300

    #45 - Create and show scratch card with verification of existence of submissions. Co-authored-by: marcelo-magalhaes <magalhaesmarcelo@outlook.com.br> Co-authored-by: aline-lv <alinelv_laureano@hotmail.com> Co-authored-by: williamelias <williameliasalves@gmail.com>

---
+++ questions/templatetags/my_templatetag.py
@@ -0,0 +1,19 @@
+from django import template
+
+from questions.models import GRATSubmission
+from groups.models import Group
+
+register = template.Library()
+@register.filter(name='filterGRAT')
+def filterGRAT(question,user):
+
+    discipline = question.session.discipline
+    group = user.student_groups.all().filter(discipline=discipline)
+    session = question.session
+
+    try:
+         submission = GRATSubmission.objects.get(session=session,group=group,question=question)
+    except Exception as e:
+        return False
+
+    return submission.score
