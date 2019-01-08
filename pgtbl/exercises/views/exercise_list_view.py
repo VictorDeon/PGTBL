507e4b60bd144fd4edfce18393825ad7aa5c8048
Victor Arnaud <victorhad@gmail.com>
2019-01-08 02:34:04 -0200

    Exercise feedback

--- pgtbl/exercises/views/exercise_list_view.py
+++ pgtbl/exercises/views/exercise_list_view.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView
 from core.permissions import PermissionMixin
 from disciplines.models import Discipline
 from exercises.forms.exercise_form import ExerciseForm
+from exercises.models import ExerciseSubmission
 from modules.models import TBLSession
 from modules.utils import get_datetimes
 from questions.models import Question
@@ -60,12 +61,40 @@ class ExerciseListView(LoginRequiredMixin,
         context['discipline'] = self.get_discipline()
         context['session'] = self.get_session()
         context['form'] = ExerciseForm()
+        context['submission'] = self.get_student_question_submissions()
         context['form1'] = AnswerQuestionForm(prefix="alternative01")
         context['form2'] = AnswerQuestionForm(prefix="alternative02")
         context['form3'] = AnswerQuestionForm(prefix="alternative03")
         context['form4'] = AnswerQuestionForm(prefix="alternative04")
 
         return context
+
+    def get_student_question_submissions(self):
+        """
+        Get the student submission for specific question.
+        """
+
+        questions = self.get_queryset()
+
+        page = self.request.GET.get("page")
+
+        if page:
+            page = int(page) - 1
+        else:
+            page = 0
+
+        submission = None
+
+        try:
+            submission = ExerciseSubmission.objects.get(
+                session=self.get_session(),
+                user=self.request.user,
+                question=questions[page]
+            )
+        except:
+            pass
+
+        return submission
 
     def get_queryset(self):
         """
