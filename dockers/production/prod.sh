a1cde5d9583fc65277cd63e861026025d7bb4806
Victor Arnaud <victorhad@gmail.com>
2019-03-21 20:49:51 -0300

    Fix dockerfile

--- dockers/production/prod.sh
+++ dockers/production/prod.sh
@@ -8,6 +8,30 @@ echo "Deleting migrations"
 find . -path "pgtbl/*/migrations/*.pyc"  -delete
 find . -path "pgtbl/*/migrations/*.py" -not -name "__init__.py" -delete
 
+echo "Waiting for database connect"
+postgres_ready() {
+python3 << END
+import sys
+import psycopg2
+import os
+try:
+    conn = psycopg2.connect(
+      dbname=os.environ.get('POSTGRES_NAME', 'pgtbl_db'),
+      user=os.environ.get('POSTGRES_USER', 'pgtbl'),
+      password=os.environ.get('POSTGRES_PASSWORD', 'pgtbl'),
+      host=os.environ.get('POSTGRES_HOST', 'db')
+    )
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+END
+}
+
+until postgres_ready; do
+  >&2 echo "PostgreSQL is not available - Waiting..."
+  sleep 1
+done
+
 echo "Creating migrations and insert into psql database"
 make migrations
 make migrate
